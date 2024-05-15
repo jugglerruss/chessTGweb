4c2dcdd229d6a2b88273344cbe0d33831b240037
Даниил <jugglerrus@gmail.com>
2024-05-15 20:16:12 +0500

    shoose side

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-ChessTG-1.0";
 const contentToCache = [
-    "Build/0070ba39a5d30e346517fa64faa8c9c2.loader.js",
+    "Build/bf5d45126b2b89cf0f19aa0492ba9715.loader.js",
     "Build/1c34e42141c854b11468c74fcb22d112.framework.js",
-    "Build/3742b0b2bfce628cffb8e36f4fd1838c.data",
-    "Build/b073e811a39b524938e19c1c63f0c3bd.wasm",
+    "Build/a9357140e09876144097c22ecea215bb.data",
+    "Build/f49e79ce0041788777f17936bb4e2976.wasm",
     "TemplateData/style.css"
 
 ];

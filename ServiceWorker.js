3f716f4bb644c626e361577e4955cbb99a00ef9c
Даниил <jugglerrus@gmail.com>
2024-05-16 14:45:39 +0500

    fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-ChessTG-1.0";
 const contentToCache = [
     "Build/bf5d45126b2b89cf0f19aa0492ba9715.loader.js",
     "Build/1c34e42141c854b11468c74fcb22d112.framework.js",
-    "Build/a9357140e09876144097c22ecea215bb.data",
+    "Build/fcbe838677ed190296186e056c4fbd05.data",
     "Build/f49e79ce0041788777f17936bb4e2976.wasm",
     "TemplateData/style.css"
 

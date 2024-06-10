a09305b53fd9e2ab07308348d057b7855307f3f9
Даниил <jugglerrus@gmail.com>
2024-06-10 15:39:19 +0500

    f

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-ChessTG-1.0";
 const contentToCache = [
     "Build/24cb0928bd13ab4dde249a1d2135a570.loader.js",
     "Build/4b7cb99a484662ca05238ce8c0c5ba97.framework.js",
-    "Build/0fcd72d04f70d066390efd39d91d525c.data",
+    "Build/00dddea54b711e4e32338a93a9c5caea.data",
     "Build/b3c6a7d9824923c47196bb80dd384a96.wasm",
     "TemplateData/style.css"
 
